e1e334f974773d8d2506515a0621acdb9d1b320e
PavloBilenko <bilenko.pavlo@gmail.com>
2024-12-19 02:58:07 +0100

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactModal from "react-modal";
 import App from "./App";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const rootElement = document.getElementById("app");
+// Встановлюємо кореневий елемент для react-modal
+ReactModal.setAppElement("#app");
 
-  if (!rootElement) {
-    throw new Error("Target container 'app' is not found in the DOM.");
-  }
+// Отримуємо DOM-елемент
+const rootElement = document.getElementById("app");
+if (!rootElement) {
+  throw new Error("Target container 'app' is not found in the DOM.");
+}
 
-  const root = ReactDOM.createRoot(rootElement);
+// Створюємо кореневий вузол і рендеримо додаток
+const root = ReactDOM.createRoot(rootElement);
 
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
